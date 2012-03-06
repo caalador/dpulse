7fc8b00f6fa8e214f9f57dca1b1c3ca27d764ccd
mikael.grankvist@gmail.com <mikael.grankvist@gmail.com@2f59b06a-8668-8fd4-9f2c-13aa59b92268>
2012-03-06 14:00:16 +0000

    Random test targets added.

--- src/org/vaadin/mgrankvi/dpulse/DataPulseApplication.java
+++ src/org/vaadin/mgrankvi/dpulse/DataPulseApplication.java
@@ -11,6 +11,7 @@ public class DataPulseApplication extends Application {
 
 	@Override
 	public void init() {
+		
 
 		final Window mainWindow = new Window("Connected");
 		setMainWindow(mainWindow);
@@ -21,31 +22,28 @@ public class DataPulseApplication extends Application {
 
 		final Controller controller = new Controller(connectionTester);
 
-		connectionTester.addConnection(new HtmlConnector("http://www.yesasia.com", "Yes asia"));
-		connectionTester.addConnection(new HtmlConnector("http://www.hs.fi", "Helsingin sanomat"));
-		connectionTester.addConnection(new HtmlConnector("http://www.amazon.co.jp", "Amazon japan"));
-		connectionTester.addConnection(new HtmlConnector("http://www.kauppalehti.fi", "Kauppalehti"));
-		connectionTester.addConnection(new HtmlConnector("http://docs.oracle.com/", "Java documentation"));
-		connectionTester.addConnection(new HtmlConnector("http://www.ebookjapan.jp/", " EBook Japan"));
 		// connectionTester.addConnection(new
-		// HtmlConnector("http://localhost:8080/Agent/test/", "Rand result"));
+		// HtmlConnector("http://www.yesasia.com", "Yes asia"));
+		// connectionTester.addConnection(new HtmlConnector("http://www.hs.fi",
+		// "Helsingin sanomat"));
+		// connectionTester.addConnection(new
+		// HtmlConnector("http://www.amazon.co.jp", "Amazon japan"));
+		// connectionTester.addConnection(new
+		// HtmlConnector("http://www.kauppalehti.fi", "Kauppalehti"));
+		// connectionTester.addConnection(new
+		// HtmlConnector("http://docs.oracle.com/", "Java documentation"));
+		// connectionTester.addConnection(new
+		// HtmlConnector("http://www.ebookjapan.jp/", " EBook Japan"));
 
-		// connectionTester.addConnection(new
-		// HtmlConnector("http://localhost:8080/Agent/test/", "Rand result"));
-		// connectionTester.addConnection(new
-		// HtmlConnector("http://localhost:8080/Agent/test/", "Rand result"));
-		// connectionTester.addConnection(new
-		// HtmlConnector("http://localhost:8080/Agent/test/", "Rand result"));
-		// connectionTester.addConnection(new
-		// HtmlConnector("http://localhost:8080/Agent/test/", "Rand result"));
-		// connectionTester.addConnection(new
-		// HtmlConnector("http://localhost:8080/Agent/test/", "Rand result"));
-		// connectionTester.addConnection(new
-		// HtmlConnector("http://localhost:8080/Agent/test/", "Rand result"));
-		// connectionTester.addConnection(new
-		// HtmlConnector("http://localhost:8080/Agent/test/", "Rand result"));
-		// connectionTester.addConnection(new
-		// HtmlConnector("http://localhost:8080/Agent/test/", "Rand result"));
+		connectionTester.addConnection(new HtmlConnector("http://localhost:8080/DPulse/random/", "Random result"));
+		connectionTester.addConnection(new HtmlConnector("http://localhost:8080/DPulse/random/", "Random result"));
+		connectionTester.addConnection(new HtmlConnector("http://localhost:8080/DPulse/random/", "Random result"));
+		connectionTester.addConnection(new HtmlConnector("http://localhost:8080/DPulse/random/", "Random result"));
+		connectionTester.addConnection(new HtmlConnector("http://localhost:8080/DPulse/random/", "Random result"));
+		connectionTester.addConnection(new HtmlConnector("http://localhost:8080/DPulse/random/", "Random result"));
+		connectionTester.addConnection(new HtmlConnector("http://localhost:8080/DPulse/random/", "Random result"));
+		connectionTester.addConnection(new HtmlConnector("http://localhost:8080/DPulse/random/", "Random result"));
+		connectionTester.addConnection(new HtmlConnector("http://localhost:8080/DPulse/random/", "Random result"));
 
 		mainWindow.addComponent(controller);
 		mainWindow.addComponent(connectionTester);
